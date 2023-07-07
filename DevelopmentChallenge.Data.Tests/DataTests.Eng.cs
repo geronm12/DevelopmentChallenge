7fbc8f0692365184a2bc9daf0609ac1779c047ad
geronm12 <gerolpz01@gmail.com>
2023-07-07 12:32:13 -0300

    arreglados test en ingles

--- DevelopmentChallenge.Data.Tests/DataTests.Eng.cs
+++ DevelopmentChallenge.Data.Tests/DataTests.Eng.cs
@@ -5,38 +5,69 @@
 
 
     [TestFixture]
-    public class DataTestsIta : TestBase, IDataTests
+    public class DataTestsEng : TestBase, IDataTests
     {
-        private Idioma idioma;
-        private FormasGeometricasService service;
+        private Idioma _idioma;
+        private FormasGeometricasService _service;
        
-        public DataTestsIta()
+        public DataTestsEng()
         {
-            idioma = new Italiano();
-            service = new FormasGeometricasService(idioma, _messageService);
+            _idioma = new Ingles();
+            _service = new FormasGeometricasService(_idioma, _messageService);
         }
-
 
         [TestCase]
         public void TestResumenListaVacia()
         {
-            Assert.AreEqual("<h1>Elenco vuoto di moduli!</h1>",
-                service.Imprimir(new List<FormaGeometrica>()));
+            Assert.AreEqual("<h1>Empty list of shapes!</h1>",
+                _service.Imprimir(new List<FormaGeometrica>()));
         }
 
-        public void TestResumenListaConMasCuadrados()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void TestResumenListaConMasTipos()
-        {
-            throw new System.NotImplementedException();
-        }
-
+        [TestCase]
         public void TestResumenListaConUnCuadrado()
         {
-            throw new System.NotImplementedException();
+            var cuadrados = new List<FormaGeometrica> { new Cuadrado(5) };
+
+            var resumen = _service.Imprimir(cuadrados);
+
+            Assert.AreEqual("<h1>Shapes report</h1> 1 Square | Area 25 | Perimeter 20 <br/>TOTAL:<br/>1 Shapes Perimeter 20 Area 25", resumen);
+        }
+
+        [TestCase]
+        public void TestResumenListaConMasCuadrados()
+        {
+            var cuadrados = new List<FormaGeometrica>
+            {
+                new Cuadrado(5),
+                new Cuadrado(1),
+                new Cuadrado(3)
+            };
+
+            var resumen = _service.Imprimir(cuadrados);
+
+            Assert.AreEqual("<h1>Shapes report</h1> 3 Squares | Area 35 | Perimeter 36 <br/>TOTAL:<br/>3 Shapes Perimeter 36 Area 35", resumen);
+        }
+
+        [TestCase]
+        public void TestResumenListaConMasTipos()
+        {
+            var formas = new List<FormaGeometrica>
+           {
+               new Cuadrado(5),
+               new Circulo(3),
+               new TrianguloEquilatero(4),
+               new Cuadrado(2),
+               new TrianguloEquilatero(9),
+               new Rectangulo(30,15),
+               new Rectangulo(20,5)
+           };
+
+            var resumen = _service.Imprimir(formas);
+
+            Assert.AreEqual(
+                "<h1>Shapes report</h1> 2 Squares | Area 29 | Perimeter 28 <br/>1 Circle | Area 7,07 | Perimeter 9,42 <br/>2 Triangles | Area 42 | Perimeter 39 <br/>2 Rectangles | Area 550 | Perimeter 140 <br/>TOTAL:<br/>7 Shapes Perimeter 216,42 Area 628,07",
+                resumen);
         }
     }
 }
+
--- DevelopmentChallenge.Data.Tests/DataTests.Ita.cs
+++ DevelopmentChallenge.Data.Tests/DataTests.Ita.cs
@@ -5,37 +5,70 @@
 
 
     [TestFixture]
-    public class DataTestsEng : TestBase, IDataTests
+    public class DataTestsIta : TestBase, IDataTests
     {
-        private Idioma idioma;
-        private FormasGeometricasService service;
+        private Idioma _idioma;
+        private FormasGeometricasService _service;
        
-        public DataTestsEng()
+        public DataTestsIta()
         {
-            idioma = new Ingles();
-            service = new FormasGeometricasService(idioma, _messageService);
+            _idioma = new Italiano();
+            _service = new FormasGeometricasService(_idioma, _messageService);
         }
+
 
         [TestCase]
         public void TestResumenListaVacia()
         {
-            Assert.AreEqual("<h1>Empty list of shapes!</h1>",
-                service.Imprimir(new List<FormaGeometrica>()));
+            Assert.AreEqual("<h1>Elenco vuoto di moduli!</h1>",
+                _service.Imprimir(new List<FormaGeometrica>()));
         }
 
-        public void TestResumenListaConMasCuadrados()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void TestResumenListaConMasTipos()
-        {
-            throw new System.NotImplementedException();
-        }
-
+        [TestCase]
         public void TestResumenListaConUnCuadrado()
         {
-            throw new System.NotImplementedException();
+            var cuadrados = new List<FormaGeometrica> { new Cuadrado(5) };
+
+            var resumen = _service.Imprimir(cuadrados);
+
+            Assert.AreEqual("<h1>Reporte de Formas</h1> 1 Cuadrado | Area 25 | Perimetro 20 <br/>TOTAL:<br/>1 Formas Perimetro 20 Area 25", resumen);
+        }
+
+        [TestCase]
+        public void TestResumenListaConMasCuadrados()
+        {
+            var cuadrados = new List<FormaGeometrica>
+            {
+                new Cuadrado(5),
+                new Cuadrado(1),
+                new Cuadrado(3)
+            };
+
+            var resumen = _service.Imprimir(cuadrados);
+
+            Assert.AreEqual("<h1>Reporte de Formas</h1> 3 Cuadrados | Area 35 | Perimetro 36 <br/>TOTAL:<br/>3 Formas Perimetro 36 Area 35", resumen);
+        }
+
+        [TestCase]
+        public void TestResumenListaConMasTipos()
+        {
+            var formas = new List<FormaGeometrica>
+           {
+               new Cuadrado(5),
+               new Circulo(3),
+               new TrianguloEquilatero(4),
+               new Cuadrado(2),
+               new TrianguloEquilatero(9),
+               new Rectangulo(30,15),
+               new Rectangulo(20,5)
+           };
+
+            var resumen = _service.Imprimir(formas);
+
+            Assert.AreEqual(
+                "<h1>Reporte de Formas</h1> 2 Cuadrados | Area 29 | Perimetro 28 <br/>1 Circulo | Area 7,07 | Perimetro 9,42 <br/>2 Triangulos | Area 42 | Perimetro 39 <br/>2 Rectangulos | Area 550 | Perimetro 140 <br/>TOTAL:<br/>7 Formas Perimetro 216,42 Area 628,07",
+                resumen);
         }
     }
 }
+
